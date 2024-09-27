1c248369b71544660f1711df248e22aa48dcdcdf
Minttu Hurme <ammsalme@users.noreply.github.com>
2024-09-27 16:04:18 +0300

    Update function

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 export default function ({type}) {
   return (req, res, next) => {
-    if (!req.is(type)) {
-      res.sendStatus(415);
-      return;
+    if (req.is(type)) {
+      return next();
     }
-    next();
+    return res.sendStatus(415);
   };
 }
